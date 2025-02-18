67d9cc8718931d679f375292e80e47b3465608ff
Jefferson Prensa <I.Jeff.Pren@gmail.com>
2025-02-18 14:23:53 +0100

    Initial Commit

---
+++ FoodLoop/FoodLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLoopApp.swift
+//  FoodLoop
+//
+//  Created by Jefferson Prensa on 18.02.25.
+//
+
+import SwiftUI
+
+@main
+struct FoodLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodLoopTests/FoodLoopTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLoopTests.swift
+//  FoodLoopTests
+//
+//  Created by Jefferson Prensa on 18.02.25.
+//
+
+import Testing
+@testable import FoodLoop
+
+struct FoodLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
